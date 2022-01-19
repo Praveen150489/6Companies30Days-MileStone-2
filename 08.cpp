e4ffb73dc3fa711709f3ed8ef8f315464955111d
PRAVEEN PRAKASH <73226469+Praveen150489@users.noreply.github.com>
2022-01-19 20:52:39 +0530

    Add files via upload

---
+++ 08.cpp
@@ -0,0 +1,13 @@
+// Count ways to Nth Stair (Order doesn't matter).
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        return (m/2 +1);
+    }
+};
